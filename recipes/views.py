0e22eb784a38321951d4a3a0d7dbd2402f54eefe
dednascimento <deividnascimento.corporativo@gmail.com>
2024-04-12 11:34:35 -0300

    - ORGANIZANDO TEMPLATES

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('CONTATO 1')
